1f32e77e4767ed7494933cda9546b1f9004fa067
AKOI2000 <alausayinka@gmail.com>
2025-12-23 01:35:42 +0100

    header url done

--- api/clients.js
+++ api/clients.js
@@ -2,6 +2,12 @@
 import pool from '../utils/db.js';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://boyidris.vercel.app'); // restrict to your frontend
+res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+if (req.method === 'OPTIONS') return res.status(200).end();
+
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
